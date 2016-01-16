d1322a8aa0c5cc4a70d778c3c6df822266d69ffa
kasunsamarawickrama <kasun2030@gmail.com>
2016-01-16 09:08:00 +0530

    Branch Model by irfan

---
+++ BankLoanSystem/BankLoanSystem/Models/BranchModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankLoanSystemTFN.Models
+{
+    public class BranchModel
+    {
+        public int BranchId { get; set; }
+        public string BranchName { get; set; }
+        public string BranchCode { get; set; }
+    }
+}
