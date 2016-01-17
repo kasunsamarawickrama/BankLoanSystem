5f1ea3e7d7dffdde572c83dd758f4d41f3460868
kasunsamarawickrama <kasun2030@gmail.com>
2016-01-17 17:05:40 +0530

    create branch controller added - piyumi

---
+++ BankLoanSystem/BankLoanSystem/Controllers/CreateBranch/CreateBranchController.cs
@@ -0,0 +1,33 @@
+﻿using BankLoanSystem.DAL;
+using BankLoanSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankLoanSystem.Controllers.CreateBranch
+{
+    public class CreateBranchController : Controller
+    {
+        // GET: CreateBranch
+        public ActionResult CreateBranch()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("CreateBranch")]
+        public ActionResult CreateBranchPost(Branch branch, int id)
+        {
+            //int id = 2;
+            BranchAccess br = new BranchAccess();
+            bool reslt = br.insertBranchDetails(branch, id);
+            if (reslt)
+            {
+                return RedirectToAction("CreateBranch");
+            }
+            return View();
+
+        }
+    }
+}
