8e3e7b43872e94ce153a10c9382e2f7037a7ba8c
kasunsamarawickrama <kasun2030@gmail.com>
2016-01-17 07:51:15 +0530

    Add RoleAccess class to DAL.

---
+++ BankLoanSystem/BankLoanSystem/DAL/RoleAccess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using BankLoanSystem.Models;
+
+namespace BankLoanSystem.DAL
+{
+    public class RoleAccess
+    {
+        public List<UserRole> GetAllUserRoles()
+        {
+            List<UserRole> userRoleList = new List<UserRole>();
+
+            using (
+                var con =
+                    new SqlConnection(
+                        ConfigurationManager.ConnectionStrings["AutoDealersConnection"].ToString()))
+            {
+                var command = new SqlCommand("spGetAllUserRole", con) { CommandType = CommandType.StoredProcedure };
+                con.Open();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        UserRole role = new UserRole
+                        {
+                            RoleId = Convert.ToInt32(reader["role_id"]),
+                            RoleName = reader["role_name"].ToString()
+                        };
+                        userRoleList.Add(role);
+                    }
+                }
+            }
+
+            return userRoleList;
+        } 
+    }
+}
