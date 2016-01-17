4ff44e1509b7d80d2ad5258cc03b971b674ae867
kasunsamarawickrama <kasun2030@gmail.com>
2016-01-17 17:59:54 +0530

    Edit Email in Model by Kanishka.

--- BankLoanSystem/BankLoanSystem/Models/Email.cs
+++ BankLoanSystem/BankLoanSystem/Models/Email.cs
@@ -1,12 +1,8 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Net;
 using System.Net.Mail;
-using System.Web;
 
-namespace Employee.Controller
+namespace BankLoanSystem.Models
 {
     public class Email
     {
