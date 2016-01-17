5ec7a06cc7ef2eff72dd698428394833777195be
kasunsamarawickrama <kasun2030@gmail.com>
2016-01-17 17:26:48 +0530

    Generate Random string added by irfan

--- BankLoanSystem/BankLoanSystem/Models/ForgotPassword.cs
+++ BankLoanSystem/BankLoanSystem/Models/ForgotPassword.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace BankLoanSystem.Models
 {
     public class ResetPassword
     {
-
+        [Display(Name = "Password")]
         [Required(ErrorMessage = "Password is required")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Confirm Password is required")]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Confirm Password")]
         [DataType(DataType.Password)]
-        [Compare("Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
 
@@ -30,5 +30,30 @@ namespace BankLoanSystem.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        public string token { get; set; }
+
+        /// <summary>
+        /// Generates random strings with a given length
+        /// </summary>
+        /// <param name="size">Size of the string</param>
+        /// <param name="lowerCase">If true, generate lowercase string</param>
+        /// <returns>Random string</returns>
+        public string GenerateRandomString(int size)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 1; i < size + 1; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+
+            token = builder.ToString();
+
+
+            return token;
+        }
     }
 }
