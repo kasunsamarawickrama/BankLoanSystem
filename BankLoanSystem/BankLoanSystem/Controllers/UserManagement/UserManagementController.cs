06d7f47e69eb6bdafab9b5ba5fec96b19b8da2fc
kasunsamarawickrama <kasun2030@gmail.com>
2016-01-18 18:44:29 +0530

    added the function by irfan

--- BankLoanSystem/BankLoanSystem/Controllers/UserManagement/UserManagementController.cs
+++ BankLoanSystem/BankLoanSystem/Controllers/UserManagement/UserManagementController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BankLoanSystem.DAL;
+using BankLoanSystem.Models;
 
 namespace BankLoanSystem.Controllers
 {
@@ -16,21 +17,38 @@ namespace BankLoanSystem.Controllers
             int typeval = (int)Session["type"];
             UserManageAccess obj1 = new UserManageAccess();
             int role = obj1.getUserRole(idval);
-            if ((typeval>0) && (idval>0)) {
+            if ((typeval > 0) && (idval > 0))
+            {
 
                 var ret = obj1.getUserByType(typeval, idval);
                 return View(ret);
             }
             else { return View(); }
-            
+
         }
+
         public ActionResult Detailsset(int id)
         {
-            TempData["rowId"]=id;
+            TempData["rowId"] = id;
 
             return RedirectToAction("Details", "UserManagement");
         }
 
+
+        [HttpPost]
+        public ActionResult UserList(int id, string clickType)
+        {
+            if (clickType.Equals("edit"))
+            {
+                Session["editId"] = id;
+                return RedirectToAction("editUser", "ManageUsers");
+
+
+            }
+
+            return View();
+
+        }
         public ActionResult Details()
         {
             int id = (int)TempData["rowId"];
@@ -44,6 +62,136 @@ namespace BankLoanSystem.Controllers
 
 
         }
+
+
+
+        public ActionResult editUserSet(int editId)
+        {
+
+            // check if he is allowable to view/edit
+
+
+            Session["editId"] = editId;
+            return RedirectToAction("editUser");
+        }
+        /// <summary>
+        /// CreatedBy : MAM. IRFAN
+        /// CreatedDate: 2016/01/13
+        /// 
+        /// Showing editable User using currentUserId and editableUserId
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// 
+
+        public ActionResult editUser()
+        {
+            int currentUserId;
+            int editUserId;
+
+            try
+            {
+                currentUserId = 1; // current user id hard coded
+                editUserId = int.Parse(Session["editId"].ToString());
+            }
+            catch (Exception)
+            {
+                return new HttpStatusCodeResult(404);
+            }
+            if (currentUserId == editUserId) { ViewBag.isSame = true; }
+            else { ViewBag.isSame = false; }
+
+
+            int companyId;
+
+            User editUser = (new UserAccess()).retreiveUserByUserId(editUserId);     //-- edit User id is hard coded
+            companyId = editUser.Company_Id;
+            // get all branches
+            List<Branch> branchesLists = (new BranchAccess()).getBranches(companyId);
+
+            // insert all branches into selectedlist
+            List<SelectListItem> branchSelectLists = new List<SelectListItem>();
+            foreach (Branch branch in branchesLists)
+            {
+                branchSelectLists.Add(new SelectListItem() { Text = branch.BranchName, Value = branch.BranchId.ToString() });
+
+            }
+
+            ViewBag.BranchId = new SelectList(branchSelectLists, "Value", "Text", editUser.BranchId);
+
+            return View(editUser);
+        }
+
+        /// <summary>
+        /// CreatedBy : MAM. IRFAN
+        /// CreatedDate: 2016/01/13
+        /// 
+        /// updating the user detail
+        /// 
+        /// argument: User
+        /// 
+        /// </summary>
+        /// <returns>update successful mesage / failed to update</returns>
+
+        [HttpPost]
+        public ActionResult editUser(User user)
+        {
+
+            int currentUserId;
+            int editUserId;
+
+            try
+            {
+                currentUserId = 1; // current user id hard coded
+                editUserId = int.Parse(Session["editId"].ToString());
+            }
+            catch (Exception)
+            {
+                return new HttpStatusCodeResult(404);
+            }
+
+            bool isUpdate;
+            if (currentUserId == editUserId)
+            {// Update the data into database
+                ViewBag.isSame = true;
+                isUpdate = (new UserAccess()).updateProfileDetails(editUserId, user.UneditUserName, user.FirstName, user.LastName, user.Email, user.PhoneNumber, DateTime.Now, user.Password);
+            }
+            else
+            {   // Update the data into database
+                ViewBag.isSame = false;
+                isUpdate = (new UserAccess()).updateUserDetails(editUserId, user.UneditUserName, user.FirstName, user.LastName, user.Email, user.PhoneNumber, user.Status, user.BranchId, DateTime.Now, user.Password);
+            }
+
+
+
+
+
+            if (isUpdate)
+                ViewBag.SuccessMsg = "Data Successfully Updated";
+            else {
+                ViewBag.ErrorMsg = "Updating failed";
+                return View(user);
+            }
+
+            user = (new UserAccess()).retreiveUserByUserId(editUserId);
+            // get all branches
+            List<Branch> branchesLists = (new BranchAccess()).getBranches(user.Company_Id);
+
+            // insert all branches into selectedlist
+            List<SelectListItem> branchSelectLists = new List<SelectListItem>();
+            foreach (Branch branch in branchesLists)
+            {
+                branchSelectLists.Add(new SelectListItem() { Text = branch.BranchName, Value = branch.BranchId.ToString() });
+
+            }
+
+
+            ViewBag.BranchId = new SelectList(branchSelectLists, "Value", "Text", user.BranchId);
+
+
+            return View(user);
+        }
+
         public ActionResult PrevDelete(int id, int lId, int roleId)
         {
             TempData["delRowId"] = id;
@@ -51,6 +199,7 @@ namespace BankLoanSystem.Controllers
             TempData["roleId"] = roleId;
             return RedirectToAction("Delete", "UserManagement");
         }
+
         public ActionResult Delete()
         {
             int id = (int)TempData["delRowId"];
@@ -65,7 +214,7 @@ namespace BankLoanSystem.Controllers
                 if (ret)
                 {
                     ViewBag.SuccessMsg = "User is successfully deleted";
-                    
+
                 }
                 else
                 {
@@ -78,6 +227,5 @@ namespace BankLoanSystem.Controllers
 
 
         }
-
     }
 }
