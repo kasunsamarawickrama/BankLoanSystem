e3fbaec69a49bed32bcd2b9a275d650622b2a82c
kasunsamarawickrama <kasun2030@gmail.com>
2016-01-18 18:12:32 +0530

    csproj added by irfan

--- BankLoanSystem/BankLoanSystem/Global.asax.cs
+++ BankLoanSystem/BankLoanSystem/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -13,6 +15,62 @@ namespace BankLoanSystem
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_Session_Start(object sender, EventArgs e)
+        {
+            //Check If it is a new session or not , if not then do the further checks
+            if (Request.Cookies["ASP.NET_SessionId"] != null || Request.Cookies["ASP.NET_SessionId"].Value.Length != 0)
+            {
+                string newSessionID = Request.Cookies["ASP.NET_SessionID"].Value;
+                //Check the valid length of your Generated Session ID
+                if (newSessionID.Length <= 24)
+                {
+                    //Log the attack details here
+                    
+                    Response.Cookies["TriedTohack"].Value = "True";
+                    
+                    throw new HttpException("Invalid Request");
+                }
+
+                //Genrate Hash key for this User,Browser and machine and match with the Entered NewSessionID
+                if (!GenerateHashKey().Substring(0,24).Equals(newSessionID.Substring(0,24)))
+                {
+                    //Log the attack details here
+                    
+                    Response.Cookies["TriedTohack"].Value = "True";
+
+                    throw new HttpException("Invalid Request");
+                }
+
+                //Use the default one so application will work as usual//ASP.NET_SessionId
+                Request.Cookies["ASP.NET_SessionId"].Value = Request.Cookies["ASP.NET_SessionId"].Value.Substring(0, 24);
+                return;
+            }
+
+        }
+
+        protected void Application_Session_End(object sender, EventArgs e)
+        {
+            //Pass the custom Session ID to the browser.
+            if (Response.Cookies["ASP.NET_SessionId"] != null)
+            {
+                Response.Cookies["ASP.NET_SessionId"].Value = Request.Cookies["ASP.NET_SessionId"].Value + GenerateHashKey();
+            }
+
+        }
+
+        private string GenerateHashKey()
+        {
+            StringBuilder myStr = new StringBuilder();
+            myStr.Append(Request.Browser.Browser);
+            myStr.Append(Request.Browser.Platform);
+            myStr.Append(Request.Browser.MajorVersion);
+            myStr.Append(Request.Browser.MinorVersion);
+            //myStr.Append(Request.LogonUserIdentity.User.Value);
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            byte[] hashdata = sha.ComputeHash(Encoding.UTF8.GetBytes(myStr.ToString()));
+            return Convert.ToBase64String(hashdata);
         }
     }
 }
