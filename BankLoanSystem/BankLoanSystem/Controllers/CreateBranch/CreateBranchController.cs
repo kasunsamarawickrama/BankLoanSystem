60ebea3f200d58ebee31880a7bca4922e5a02bc8
kasunsamarawickrama <kasun2030@gmail.com>
2016-01-18 08:20:36 +0530

    CreateBranch controller by Kanishka.

--- BankLoanSystem/BankLoanSystem/Controllers/CreateBranch/CreateBranchController.cs
+++ BankLoanSystem/BankLoanSystem/Controllers/CreateBranch/CreateBranchController.cs
@@ -10,23 +10,42 @@ namespace BankLoanSystem.Controllers.CreateBranch
 {
     public class CreateBranchController : Controller
     {
+        private static string _type = "";
+        private static Company _company = null;
         // GET: CreateBranch
-        public ActionResult CreateBranch()
+        public ActionResult CreateBranch(string type)
         {
+            if (type == "CompanyEmployee")
+            {
+                ViewBag.Type = "CompanyEmployee";
+                _type = "CompanyEmployee";
+                _company = (Company)TempData["Company"];
+            }
             return View();
         }
         [HttpPost]
         [ActionName("CreateBranch")]
         public ActionResult CreateBranchPost(Branch branch, int id)
         {
-            //int id = 2;
-            BranchAccess br = new BranchAccess();
-            bool reslt = br.insertBranchDetails(branch, id);
-            if (reslt)
+            if (_type == "CompanyEmployee")
             {
-                return RedirectToAction("CreateBranch");
+                CompanyBranchModel comBra = new CompanyBranchModel();
+                comBra.Company = _company;
+                comBra.MainBranch = branch;
+                TempData["CompanyMainBranch"] = comBra;
+                return RedirectToAction("CreateFirstSuperUser", "CreateUser");
             }
-            return View();
+            else
+            {
+                //int id = 2;
+                BranchAccess br = new BranchAccess();
+                bool reslt = br.insertBranchDetails(branch, id);
+                if (reslt)
+                {
+                    return RedirectToAction("UserDashBoard", "DashBoard");
+                }
+                return View();
+            }
 
         }
     }
