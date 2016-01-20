752b20a4a2084b0b6d060dafac3ecd0027351e4f
kasunsamarawickrama <kasun2030@gmail.com>
2016-01-20 14:28:23 +0530

    Add HtmlExtension.cs - Kanishka

---
+++ BankLoanSystem/BankLoanSystem/Code/HtmlExtension.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace System.Web.Mvc.Html
+{
+
+    public static class ChqHtmlHelperExtensions
+    {
+        public static MvcHtmlString ChqLabelFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression, object htmlAttributes)
+        {
+            var metadata = ModelMetadata.FromLambdaExpression(expression, html.ViewData);
+            string resolvedLabelText = metadata.DisplayName ?? metadata.PropertyName;
+            if (metadata.IsRequired)
+            {
+                resolvedLabelText += "*";
+            }
+            return html.LabelFor<TModel, TValue>(expression, resolvedLabelText, htmlAttributes);
+        }
+    }
+}
+
+namespace BankLoanSystem.Code
+{
+
+    public static class HtmlExtensions
+    {
+        [SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "This is an appropriate nesting of generic types")]
+        public static MvcHtmlString LabelForRequired<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression, string labelText = "")
+        {
+            return LabelHelper(html,
+                ModelMetadata.FromLambdaExpression(expression, html.ViewData),
+                ExpressionHelper.GetExpressionText(expression), labelText);
+        }
+
+        private static MvcHtmlString LabelHelper(HtmlHelper html,
+           ModelMetadata metadata, string htmlFieldName, string labelText)
+        {
+            if (string.IsNullOrEmpty(labelText))
+            {
+                labelText = metadata.DisplayName ?? metadata.PropertyName ?? htmlFieldName.Split('.').Last();
+            }
+
+            if (string.IsNullOrEmpty(labelText))
+            {
+                return MvcHtmlString.Empty;
+            }
+
+            bool isRequired = false;
+
+            if (metadata.ContainerType != null)
+            {
+                isRequired = metadata.ContainerType.GetProperty(metadata.PropertyName)
+                                .GetCustomAttributes(typeof(RequiredAttribute), false)
+                                .Length == 1;
+            }
+
+            TagBuilder tag = new TagBuilder("label");
+            tag.Attributes.Add(
+                "for",
+                TagBuilder.CreateSanitizedId(
+                    html.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(htmlFieldName)
+                )
+            );
+
+            if (isRequired)
+                tag.Attributes.Add("class", "label-required");
+
+            tag.SetInnerText(labelText);
+
+            var output = tag.ToString(TagRenderMode.Normal);
+
+
+            if (isRequired)
+            {
+                var asteriskTag = new TagBuilder("span");
+                asteriskTag.Attributes.Add("class", "required");
+                asteriskTag.SetInnerText("*");
+                output += asteriskTag.ToString(TagRenderMode.Normal);
+            }
+            return MvcHtmlString.Create(output);
+        }
+    }
+}
