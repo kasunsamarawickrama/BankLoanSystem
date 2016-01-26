1a8bce03d7a08f458145066c7521f0190b86aa5a
kasunsamarawickrama <kasun2030@gmail.com>
2016-01-26 09:19:21 +0530

    SetupProcess committed by irfan

---
+++ BankLoanSystem/BankLoanSystem/Controllers/SetupProcess/SetupProcessController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankLoanSystem.Controllers.SetupProcess
+{
+    public class SetupProcessController : Controller
+    {
+        // GET: SetupProcess
+        public ActionResult Step1()
+        {
+            return View();
+        }
+    }
+}
