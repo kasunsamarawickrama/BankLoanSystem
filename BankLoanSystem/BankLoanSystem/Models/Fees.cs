91b17cd809c3580ee9e0db7bae820a1dd71c1763
kasunsamarawickrama <kasun2030@gmail.com>
2016-02-08 14:29:52 +0530

    fees - kasun

--- BankLoanSystem/BankLoanSystem/Models/Fees.cs
+++ BankLoanSystem/BankLoanSystem/Models/Fees.cs
@@ -8,22 +8,29 @@ namespace BankLoanSystem.Models
 {
     public class Fees
     {
+        public int LoanId { get; set; }
+
         [Display(Name = "Advance")]
         public int AdvanceId { get; set; }
         public string AdvanceAmount { get; set; }
-        public DateTime AdvanceDue { get; set; }
-        public string AdvanceReminder { get; set; }
+        public string AdvanceDue { get; set; }
+        public string AdvanceDueDate { get; set; }
+        public string AdvanceLenderEmail { get; set; }
+        public string AdvanceDealerEmail { get; set; }
 
         [Display(Name = "Monthly Loan")]
         public int MonthlyLoanId { get; set; }
         public string MonthlyLoanAmount { get; set; }
-        public DateTime MonthlyLoanDue { get; set; }
-        public string MonthlyLoanReminder { get; set; }
+        public string MonthlyLoanDue { get; set; }
+        public string MonthlyLoanDueDate { get; set; }
+        public string MonthlyLoanLenderEmail { get; set; }
+        public string MonthlyLoanDealerEmail { get; set; }
 
         [Display(Name = "Lot Inspection")]
         public int LotInspectionId { get; set; }
         public string LotInspectionAmount { get; set; }
-        public DateTime LotInspectionDue { get; set; }
-        public string LotInspectionReminder { get; set; }
+        public string LotInspectionDue { get; set; }
+        public string LotInspectionLenderEmail { get; set; }
+        public string LotInspectionDealerEmail { get; set; }
     }
 }
