34df60834fb582ffcfc4221cb5e39d02a26ac9d3
kasunsamarawickrama <kasun2030@gmail.com>
2016-02-11 17:02:05 +0530

    committed by piyumi

--- BankLoanSystem/BankLoanSystem/DAL/TitleAccess.cs
+++ BankLoanSystem/BankLoanSystem/DAL/TitleAccess.cs
@@ -39,17 +39,23 @@ namespace BankLoanSystem.DAL
                         while (reader.Read())
                         {
                             obj1 = new Title();
+                            obj1.LoanId = int.Parse(reader["loan_id"].ToString());
                             obj1.IsTitleTrack = bool.Parse(reader["is_title_tracked"].ToString());
                             if (obj1.IsTitleTrack)
                             {
 
                                 obj1.TitleAcceptMethod = reader["title_accept_method"].ToString();
+                                obj1.ReceivedTimeLimit = reader["title_received_time_period"].ToString();
+                                obj1.RemindEmail = reader["auto_remind_email"].ToString();
+                                
                             }
-
-
-                            obj1.ReceivedTimeLimit = reader["title_received_time_period"].ToString();
-                            obj1.RemindEmail = reader["auto_remind_email"].ToString();
+                            else
+                            {
+                                LoanSetupAccess st = new LoanSetupAccess();
+                                obj1.RemindEmail = st.getAutoRemindEmailByLoanId(obj1.LoanId);
+                            }
                             obj1.IsReceipRequired = bool.Parse(reader["is_receipt_required"].ToString());
+
                             if (obj1.IsReceipRequired)
                             {
 
@@ -64,7 +70,7 @@ namespace BankLoanSystem.DAL
                             {
                                 obj1.NeedScanCopy = true;
                             }
-                            obj1.LoanId = int.Parse(reader["loan_id"].ToString());
+                            
 
 
                         }
@@ -104,10 +110,20 @@ namespace BankLoanSystem.DAL
                         cmd.CommandType = CommandType.StoredProcedure;
 
                         cmd.Parameters.Add("@is_title_tracked", SqlDbType.Bit).Value = title.IsTitleTrack;
-                        cmd.Parameters.Add("@title_accept_method", SqlDbType.VarChar).Value = title.TitleAcceptMethod;
-                        cmd.Parameters.Add("@title_received_time_period", SqlDbType.VarChar).Value = title.ReceivedTimeLimit;
+                        if (title.IsTitleTrack)
+                        {
+                            cmd.Parameters.Add("@title_accept_method", SqlDbType.VarChar).Value = title.TitleAcceptMethod;
+                            cmd.Parameters.Add("@title_received_time_period", SqlDbType.VarChar).Value = title.ReceivedTimeLimit;
 
-                        cmd.Parameters.Add("@auto_remind_email", SqlDbType.VarChar).Value = title.RemindEmail;
+                            cmd.Parameters.Add("@auto_remind_email", SqlDbType.VarChar).Value = title.RemindEmail;
+                        }
+                        else
+                        {
+                            cmd.Parameters.Add("@title_accept_method", SqlDbType.VarChar).Value = null;
+                            cmd.Parameters.Add("@title_received_time_period", SqlDbType.VarChar).Value = null;
+
+                            cmd.Parameters.Add("@auto_remind_email", SqlDbType.VarChar).Value = null;
+                        }
 
                         cmd.Parameters.Add("@is_receipt_required", SqlDbType.Bit).Value = title.IsReceipRequired;
                         if (title.IsReceipRequired)
@@ -115,7 +131,10 @@ namespace BankLoanSystem.DAL
 
                             cmd.Parameters.Add("receipt_required_method", SqlDbType.VarChar).Value = title.ReceiptRequiredMethod;
                         }
-
+                        else
+                        {
+                            cmd.Parameters.Add("receipt_required_method", SqlDbType.VarChar).Value = null;
+                        }
                         cmd.Parameters.Add("@loan_id", SqlDbType.Int).Value = title.LoanId;
 
 
