8e9eb3a938661ba09129d19ae36e88ec1413b0ac
kasunsamarawickrama <kasun2030@gmail.com>
2016-02-23 13:32:05 +0530

    Unit - kasun

--- BankLoanSystem/BankLoanSystem/Models/Unit.cs
+++ BankLoanSystem/BankLoanSystem/Models/Unit.cs
@@ -11,40 +11,40 @@ namespace BankLoanSystem.Models
     public int UnitId { get; set; }
 
         [Required(ErrorMessage = "Vehicle Identification Number is required.")]
-        public String IdentificationNumber { get; set; }
+        public string IdentificationNumber { get; set; }
 
         [Required(ErrorMessage = "Year is required.")]
         public int Year { get; set; }
 
-        public String Make { get; set; }
+        public string Make { get; set; }
 
-        public String Model { get; set; }
+        public string Model { get; set; }
 
-        public String Trim { get; set; }
+        public string Trim { get; set; }
 
-        public Double Miles { get; set; }
+        public double Miles { get; set; }
 
-        public String Color { get; set; }
+        public string Color { get; set; }
 
         public bool NewOrUsed { get; set; }
 
-        public Double Length { get; set; }
+        public double Length { get; set; }
 
-        public String HitchStyle { get; set; }
+        public string HitchStyle { get; set; }
 
-        public Double Speed { get; set; }
+        public double Speed { get; set; }
 
-        public String TrailerId { get; set; }
+        public string TrailerId { get; set; }
 
-        public String EngineSerial { get; set; }
+        public string EngineSerial { get; set; }
 
-        public Double Cost { get; set; }
+        public double Cost { get; set; }
 
-        public Double AdvanceAmount { get; set; }
+        public double AdvanceAmount { get; set; }
 
         public bool IsTitleReceived { get; set; }
 
-        public String Note { get; set; }
+        public string Note { get; set; }
 
         public DateTime AdvanceDate { get; set; }
 
