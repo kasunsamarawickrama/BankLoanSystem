118130563164267c4b3e0f8d208af75c7bb34bd3
kasunsamarawickrama <kasun2030@gmail.com>
2016-02-23 16:56:07 +0530

    loan info added by irfan

--- BankLoanSystem/BankLoanSystem/Controllers/AddUnit/AddUnitController.cs
+++ BankLoanSystem/BankLoanSystem/Controllers/AddUnit/AddUnitController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using BankLoanSystem.DAL;
+using BankLoanSystem.Models;
+using System.Web.Mvc;
 
 namespace BankLoanSystem.Controllers.AddUnit
 {
@@ -20,6 +22,27 @@ namespace BankLoanSystem.Controllers.AddUnit
 
 
 
+            return View();
+        }
+
+
+        public ActionResult LoanInfo()
+        {
+            int userId = 57;
+            int loanId = 184;
+
+            User user = (new UserAccess()).retreiveUserByUserId(userId);
+            ViewBag.Username = user.UserName;
+            BranchAccess ba = new BranchAccess();
+            ViewBag.Branch = (ba.getBranchByBranchId(user.BranchId)).BranchName;
+            ViewBag.roleId = user.RoleId;
+            // get the Company type for front end view
+            int comType = ba.getCompanyTypeByUserId(userId);
+            ViewBag.loanCompanyType = (comType == 1) ?   "Dealer" : "Lender" ;
+
+            LoanSetupStep1 loanSetupStep1 = (new LoanSetupAccess()).GetLoanStepOne(loanId);
+            NonRegBranch nonRegBranch = ba.getNonRegBranchByNonRegBranchId(loanSetupStep1.nonRegisteredBranchId);
+            ViewBag.loanBranchName = nonRegBranch.BranchName + "-" + nonRegBranch.BranchAddress1;
             return View();
         }
     }
