75997c08c8add900b3428b3187951925ff36e757
kasunsamarawickrama <kasun2030@gmail.com>
2016-02-24 16:01:05 +0530

    unit - kasun

--- BankLoanSystem/BankLoanSystem/Models/Unit.cs
+++ BankLoanSystem/BankLoanSystem/Models/Unit.cs
@@ -9,6 +9,7 @@ namespace BankLoanSystem.Models
 
         [Display(Name = "Identification Number")]
         [Required(ErrorMessage = "Vehicle Identification Number is required.")]
+        [RegularExpression("[A-HJ-NPR-Z0-9]{13}[0-9]{4}", ErrorMessage = "Invalid Vehicle Identification Number Format.")]
         public string IdentificationNumber { get; set; }
 
         [Required(ErrorMessage = "Year is required.")]
