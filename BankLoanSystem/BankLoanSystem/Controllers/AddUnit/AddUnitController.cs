850100097f9b1f6fe7e760d0b6aa6c85213a2886
kasunsamarawickrama <kasun2030@gmail.com>
2016-02-24 11:03:39 +0530

    add unit - kasun

--- BankLoanSystem/BankLoanSystem/Controllers/AddUnit/AddUnitController.cs
+++ BankLoanSystem/BankLoanSystem/Controllers/AddUnit/AddUnitController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BankLoanSystem.DAL;
+using BankLoanSystem.Models;
 
 namespace BankLoanSystem.Controllers.AddUnit
 {
@@ -12,18 +14,28 @@ namespace BankLoanSystem.Controllers.AddUnit
         public ActionResult AddUnit()
         {
             Session["userId"] = 2;
+            int loanId = 182;
 
-            ViewBag.ListOfUnitTypes = new SelectList(new[]
-            {
-            new { Value = "1", Text = "Vehicle" },
-            new { Value = "2", Text = "RV" },
-            new { Value = "3", Text = "Camper" },
-            new { Value = "4", Text = "ATV" },
-            new { Value = "5", Text = "Boat" },
-            new { Value = "6", Text = "Motorcycle" },
-            new { Value = "7", Text = "Snowmobile" },
-            new { Value = "8", Text = "Heavy Equipment" },
-            }, "Value", "Text");
+            LoanSetupStep1 loanDetails = new LoanSetupStep1();
+            loanDetails = (new LoanSetupAccess()).GetLoanStepOne(loanId);
+
+            List<SelectListItem> ListOfUnitTypes = new List<SelectListItem>();
+
+            int id = 0;
+            string name = "";
+
+            foreach (var type in loanDetails.selectedUnitTypes) {
+
+                id = type.unitTypeId;
+                name = type.unitTypeName;
+
+                ListOfUnitTypes.Add(new SelectListItem
+                {
+                    Value = id.ToString(),
+                    Text = name
+                });
+            }
+            ViewBag.ListOfUnitTypes = ListOfUnitTypes;
 
             return View();
         }
