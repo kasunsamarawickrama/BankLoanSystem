2822f1b7d3b1ad71ba1cd37905de8f8ad4a7c422
kasunsamarawickrama <kasun2030@gmail.com>
2016-03-03 17:55:27 +0530

    committed by irfan

--- BankLoanSystem/BankLoanSystem/scripts/CommonFunctions.js
+++ BankLoanSystem/BankLoanSystem/scripts/CommonFunctions.js
@@ -2,7 +2,7 @@
     // createdBy :: MAM.IRFAN
     // if you want to modify this functionality ovewrite in your html file, don't edit in this file
 
-    // all input type numbers are tex align to left
+    // all input type numbers are tex align to right
     $(':input[type="number"]').css('text-align', 'right');
 
     // clear error messages if you use ValidationMaessageFor function to handle error
