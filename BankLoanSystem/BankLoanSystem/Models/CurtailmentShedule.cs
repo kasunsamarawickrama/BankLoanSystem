e1b7a806342719c7fc8944aa6c27c7c4bbd1bc9e
kasunsamarawickrama <kasun2030@gmail.com>
2016-03-18 16:02:32 +0530

    Committed by irfan

--- BankLoanSystem/BankLoanSystem/Models/CurtailmentShedule.cs
+++ BankLoanSystem/BankLoanSystem/Models/CurtailmentShedule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -31,5 +32,21 @@ namespace BankLoanSystem.Models
         public DateTime PayDate { get; set; }
         public DateTime DueDate { get; set; }
         public List<CurtailmentShedule> CurtailmentScheduleInfoModel { get; set; }
+
+
+
+
+        [RegularExpression(@"^[-a-zA-Z0-9]*", ErrorMessage = "Invalid VIN/HIN/Serial No")]
+        public string IdentificationNumber { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Year must be a positive value.")]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "year must contain only digits")]
+        public string Year { get; set; }
+
+        [RegularExpression(@"^[-a-zA-Z0-9]*", ErrorMessage = "Invalid Make")]
+        public string Make { get; set; }
+
+        [RegularExpression(@"^[-a-zA-Z0-9]*", ErrorMessage = "Invalid Model")]
+        public string Model { get; set; }
     }
 }
