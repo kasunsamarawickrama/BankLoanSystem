c1e5fcf83c20c72cbdf23d8117df17d3a53dc1ec
kasunsamarawickrama <kasun2030@gmail.com>
2016-03-27 04:32:37 -0500

    updated by nadeeka

--- BankLoanSystem/BankLoanSystem/Controllers/SetupProcess/SetupProcessController.cs
+++ BankLoanSystem/BankLoanSystem/Controllers/SetupProcess/SetupProcessController.cs
@@ -57,7 +57,8 @@ namespace BankLoanSystem.Controllers.SetupProcess
                 else
                 {
                     //return RedirectToAction("UserLogin", "Login", new { lbl = "Your Session Expired" });
-                    filterContext.Controller.TempData.Add("UserLogin", "Login");
+                    //filterContext.Controller.TempData.Add("UserLogin", "Login");
+                    filterContext.Result = new RedirectResult("~/Login/UserLogin");
                 }
             }
             catch(Exception e)
