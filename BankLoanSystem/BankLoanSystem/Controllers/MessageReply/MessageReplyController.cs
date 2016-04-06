5ec66c1d2062a4b73168db071d6b8c1a1d810476
asankatfn <asanka@thefuturenet.com>
2016-04-06 18:08:08 +0530

    asanka

---
+++ BankLoanSystem/BankLoanSystem/Controllers/MessageReply/MessageReplyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using BankLoanSystem.DAL;
+using BankLoanSystem.Models;
+using System.Collections.Generic;
+
+namespace BankLoanSystem.Controllers.MessageReply
+{
+    public class MessageReplyController : Controller
+    {
+        // GET: MessageReply
+        public ActionResult MessageReply()
+        {
+            List<Models.UserRequest> result = new List<Models.UserRequest>();
+
+            UserRequestAccess userreques = new UserRequestAccess();
+            result = userreques.SelectDatalistForAnswer(154);
+            return View(result);
+        }
+
+        [HttpPost]
+        public ActionResult MessageReply(UserRequest userrequest)
+        {
+            return View();
+        }
+
+    }
+}
