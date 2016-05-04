5b92d1f83c8953e54b5c60e460265757a4c0f92a
kasun <kasun2030@gmail.com>
2016-05-04 13:31:03 +0530

    renew loan

--- BankLoanSystem/BankLoanSystem/Controllers/UserManagement/UserManagementController.cs
+++ BankLoanSystem/BankLoanSystem/Controllers/UserManagement/UserManagementController.cs
@@ -814,7 +814,19 @@ namespace BankLoanSystem.Controllers
                     return RedirectToAction("UserLogin", "Login");
                 }
             }
-           else if(!string.IsNullOrEmpty(type)) 
+           else if (!string.IsNullOrEmpty(type) && type.Contains("aticno"))
+            {
+                detail = (new UnitAccess()).GetPermisssionGivenLoanwithBranchDeatils(userData.UserId, userData.Company_Id, userData.BranchId, userData.RoleId);
+                if (detail != null)
+                {
+                    Session["detail"] = detail;
+                }
+                else
+                {
+                    return RedirectToAction("UserLogin", "Login");
+                }
+            }
+            else if(!string.IsNullOrEmpty(type)) 
            {
                 detail = (new UnitAccess()).GetPermisssionGivenLoanwithBranchDeatils(userData.UserId, userData.Company_Id, userData.BranchId, userData.RoleId);
                 if (detail == null)
@@ -985,6 +997,11 @@ namespace BankLoanSystem.Controllers
                 ViewBag.type = "EditLoan";
                 return PartialView(loanSelection);
             }
+            else if (type == "aticno") 
+            {
+                ViewBag.type = "RenewLoan";
+                return PartialView(loanSelection);
+            }
             return PartialView(loanSelection);
         }
 
@@ -1087,6 +1104,8 @@ namespace BankLoanSystem.Controllers
                     //for edit loan
                     finalSelectedLoan.CurrentLoanStatus = l.CurrentLoanStatus;
                     finalSelectedLoan.CreatedDate = l.CreatedDate;
+                    finalSelectedLoan.StartDate = l.startDate;
+                    finalSelectedLoan.MaturityDate = l.maturityDate;
                     finalSelectedLoan.LoanAmount = l.loanAmount;
                     foreach (var nrbr in list3.NonRegBranchList)
                     {
@@ -1117,6 +1136,11 @@ namespace BankLoanSystem.Controllers
                         Session["loanDashboardEditLoan"] = finalSelectedLoan;
 
                     }
+                    else if ((string)Session["popUpSelectionType"] == "aticno")
+                    {
+                        Session["loanDashboardRenewLoan"] = finalSelectedLoan;
+
+                    }
                     else {
                         Session["loanDashboard"] = finalSelectedLoan;
                     }
@@ -1134,6 +1158,10 @@ namespace BankLoanSystem.Controllers
             else if ((string)Session["popUpSelectionType"] == "tidenaol")
             {
                 return RedirectToAction("EditLoan");
+            }
+            else if ((string)Session["popUpSelectionType"] == "aticno")
+            {
+                return RedirectToAction("RenewLoan","LoanManagement");
             }
             else
             {
