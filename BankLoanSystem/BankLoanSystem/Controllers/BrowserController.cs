13cb1cecbe3217c857dbb42b1bf0f84083eb7d5e
kasunsamarawickrama <kasun2030@gmail.com>
2016-05-30 15:35:04 +0530

    committed by irfan

---
+++ BankLoanSystem/BankLoanSystem/Controllers/BrowserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankLoanSystem.Controllers
+{
+    public class BrowserController : Controller
+    {
+        // GET: Browser
+        public ActionResult Support()
+        {
+            return View();
+        }
+    }
+}
