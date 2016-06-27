8d7a5a043da528ab359cbd1609f8f859b7487fe0
kasun <kasun2030@gmail.com>
2016-06-27 14:26:26 +0530

    captcha

--- BankLoanSystem/BankLoanSystem/Controllers/UserManagement/UserManagementController.cs
+++ BankLoanSystem/BankLoanSystem/Controllers/UserManagement/UserManagementController.cs
@@ -7,6 +7,12 @@ using BankLoanSystem.Models;
 using BankLoanSystem.Code;
 using System.Data;
 
+
+using SRVTextToImage;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
 namespace BankLoanSystem.Controllers
 {
     public class UserManagementController : Controller
@@ -1675,7 +1681,7 @@ namespace BankLoanSystem.Controllers
                 return RedirectToAction("UserLogin", "Login");
             }
         }
-
+        //edited kasun 2016/6/27
         public ActionResult UserRequestMessage()
         {
             int userrole = userData.RoleId;
@@ -1684,58 +1690,91 @@ namespace BankLoanSystem.Controllers
             return PartialView();
         }
 
+        //edited kasun 2016/6/27
         [HttpPost]
         [ActionName("UserRequestMessage")]
         public ActionResult UserRequestMessagePost(UserRequest userReq)
         {
-            string loancod = "";
-            string page_nam = "";
-            userReq.company_id = userData.Company_Id;
-            userReq.branch_id = userData.BranchId;
-            userReq.user_id = userData.UserId;
-            userReq.role_id = userData.RoleId;
-            if (Session["loanCode"] != null)
+            if (this.Session["CaptchaImageText"].ToString() == userReq.captcha)
             {
-                loancod = Session["loanCode"].ToString();
-            }
-            if (Session["pagetitle"] != null)
-            {
-                page_nam = Session["pagetitle"].ToString();
-            }
-            userReq.loan_code = loancod;
-            userReq.page_name = page_nam;
-            userReq.topic = userReq.topic;
-            userReq.message = userReq.message;
-            userReq.priority_level = "high";
+                string loancod = "";
+                string page_nam = "";
+                userReq.company_id = userData.Company_Id;
+                userReq.branch_id = userData.BranchId;
+                userReq.user_id = userData.UserId;
+                userReq.role_id = userData.RoleId;
+                if (Session["loanCode"] != null)
+                {
+                    loancod = Session["loanCode"].ToString();
+                }
+                if (Session["pagetitle"] != null)
+                {
+                    page_nam = Session["pagetitle"].ToString();
+                }
+                userReq.loan_code = loancod;
+                userReq.page_name = page_nam;
+                userReq.topic = userReq.topic;
+                userReq.message = userReq.message;
+                userReq.priority_level = "high";
 
-            UserRequestAccess userreqAccsss = new UserRequestAccess();
-            int reslt = userreqAccsss.InsertUserRequest(userReq);
-            if (reslt >= 0)
-            {
-                string body = "User Name     :" + userData.UserName + "< br />" +
-                              "Position      :" + (string)Session["searchType"] + "< br />" +
-                              "Company       :" + userData.CompanyName + "< br />" +
-                              "Branch        :" + userData.BranchName + "< br />" +
-                              "Loan          :" + loancod + "< br />" +
-                              "Date and Time :" + DateTime.Now + "< br />" +
-                              "Title         :" + userReq.topic + "< br />" +
-                              "Message       :" + userReq.message + "< br />" +
-                              "Page          :" + page_nam + "< br />";
+                UserRequestAccess userreqAccsss = new UserRequestAccess();
+                int reslt = userreqAccsss.InsertUserRequest(userReq);
+                if (reslt >= 0)
+                {
+                    string body = "User Name     :" + userData.UserName + "< br />" +
+                                  "Position      :" + (string)Session["searchType"] + "< br />" +
+                                  "Company       :" + userData.CompanyName + "< br />" +
+                                  "Branch        :" + userData.BranchName + "< br />" +
+                                  "Loan          :" + loancod + "< br />" +
+                                  "Date and Time :" + DateTime.Now + "< br />" +
+                                  "Title         :" + userReq.topic + "< br />" +
+                                  "Message       :" + userReq.message + "< br />" +
+                                  "Page          :" + page_nam + "< br />";
 
-                Email email = new Email("asanka@thefuturenet.com");
-                email.SendMail(body, "User Request From Dealer Floor Plan Management Software");
+                    Email email = new Email("asanka@thefuturenet.com");
+                    email.SendMail(body, "User Request From Dealer Floor Plan Management Software");
 
-                ViewBag.SuccessMsg = "Response will be delivered to your program inbox";
-                return RedirectToAction("UserRequestMessage", "UserManagement");
+                    ViewBag.SuccessMsg = "Response will be delivered to your program inbox";
+                    return RedirectToAction("UserRequestMessage", "UserManagement");
+                }
+                else
+                {
+                    ViewBag.SuccessMsg = "Error Occured";
+                    return RedirectToAction("UserRequestMessage");
+                }
             }
-            else
-            {
-                ViewBag.SuccessMsg = "Error Occured";
+            else {
+                TempData["message"] = userReq.message;
+                TempData["topic"] = userReq.message;
                 return RedirectToAction("UserRequestMessage");
             }
-            return View();
+
+
+           
         }
 
+        /// <summary>
+        /// CreatedBy : Kasun Samarawickrama
+        /// CreatedDate: 2016/06/27
+        /// 
+        /// This action for get Captcha Image
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpGet]
+        [OutputCache(NoStore = true, Duration = 0, VaryByParam = "None")] // This is for output cache false
+        public FileResult GetCaptchaImage()
+        {
+            CaptchaRandomImage CI = new CaptchaRandomImage();
+            this.Session["CaptchaImageText"] = CI.GetRandomString(5); // here 5 means I want to get 5 char long captcha
+                                                                      //CI.GenerateImage(this.Session["CaptchaImageText"].ToString(), 300, 75);
+                                                                      // Or We can use another one for get custom color Captcha Image 
+            CI.GenerateImage(this.Session["CaptchaImageText"].ToString(), 150, 50, Color.Black, Color.LightBlue);
+            MemoryStream stream = new MemoryStream();
+            CI.Image.Save(stream, ImageFormat.Png);
+            stream.Seek(0, SeekOrigin.Begin);
+            return new FileStreamResult(stream, "image/png");
+        }
         /// <summary>
         /// CreatedBy: Asanka Senarathna
         /// CreatedDate: 30/31/2016
